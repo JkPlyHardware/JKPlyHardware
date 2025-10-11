0acdd7df566e9fdc85b743be35e13da1f3ec7423
JkPlyHardware <jkplyhardwareandfurnitureworks@gmail.com>
2025-10-11 22:06:06 +0530

    script.js

--- script.js
+++ script.js
@@ -1,39 +1,50 @@
 window.onload = function () {
-            setTimeout(function () {
-                document.getElementById("offerModal").style.display = "block";
-            }, 3000);
-        };
+    setTimeout(function () {
+        document.getElementById("offerModal").style.display = "block";
+    }, 3000);
+};
 
-        function closeOffer() {
-            document.getElementById("offerModal").style.display = "none";
-        }
+function closeOffer() {
+    document.getElementById("offerModal").style.display = "none";
+}
 
-        window.onclick = function (event) {
-            let modal = document.getElementById("offerModal");
-            if (event.target == modal) {
-                modal.style.display = "none";
-            }
-        }
-        document.addEventListener("contextmenu", e => e.preventDefault()); // disable right click
-        document.onkeydown = function (e) {
-            if (e.ctrlKey && (e.key === "u" || e.key === "s" || e.key === "i")) {
-                return false; // disable Ctrl+U, Ctrl+S, Ctrl+Shift+I
-            }
-        }
-       
+window.onclick = function (event) {
+    let modal = document.getElementById("offerModal");
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+document.addEventListener("contextmenu", e => e.preventDefault()); 
+document.addEventListener("keydown", function (e) {
 
-        const scrollBtn = document.getElementById("scrollUpBtn");
+    if (e.ctrlKey && e.shiftKey && e.key.toLowerCase() === "i") {
+        alert("Element is disabled");
+        e.preventDefault();
+    }
 
-        window.onscroll = function () {
-            if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
-                scrollBtn.style.display = "block";
-                scrollBtn.classList.add("show");
-            } else {
-                scrollBtn.style.display = "none";
-                scrollBtn.classList.remove("show");
-            }
-        };
+    if (e.ctrlKey && e.shiftKey && e.key.toLowerCase() === "u") {
+        alert("Element is disabled");
+        e.preventDefault();
+    }
 
-        scrollBtn.onclick = function () {
-            window.scrollTo({ top: 0, behavior: 'smooth' });
-        };
+    if (e.key === "F12") {
+        alert("Source code is disabled");
+        e.preventDefault();
+    }
+});
+
+const scrollBtn = document.getElementById("scrollUpBtn");
+
+window.onscroll = function () {
+    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+        scrollBtn.style.display = "block";
+        scrollBtn.classList.add("show");
+    } else {
+        scrollBtn.style.display = "none";
+        scrollBtn.classList.remove("show");
+    }
+};
+
+scrollBtn.onclick = function () {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+};
